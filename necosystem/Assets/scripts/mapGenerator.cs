2726f445bc6d1f81eb1b77cff16a611db35863c2
nicko <nickrev55@gmail.com>
2023-08-10 16:33:50 +0100

    colourmap gen and mesh gen implementation
    - colours get generated based on height
    - mesh generation added
    - mesh has LOD capability to reduce no. of vertices being loaded

--- necosystem/Assets/scripts/mapGenerator.cs
+++ necosystem/Assets/scripts/mapGenerator.cs
@@ -4,16 +4,73 @@ using UnityEngine;
 
 public class mapGenerator : MonoBehaviour
 {
-    public int mapWidth;
-    public int mapHeight;
+    public enum DrawMode { NoiseMap, ColourMap, Mesh };
+    public DrawMode drawMode;
+
+    const int mapChunkSize = 241;
+    [Range(0,6)]
+    public int levelOfDetail;
     public float noiseScale;
+    
+    public int octaves;
+    [Range(0,1)]
+    public float persistance;
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+
+    public float meshHeightMultiplier;
+    public AnimationCurve meshHeightCurve;
+
     public bool autoUpdate;
+
+    public TerrainType[] regions;
 
     public void generateMap()
     {
-        float[,] noiseMap = noise.generateNoiseMap(mapWidth, mapHeight, noiseScale); //pass variables into noise generation function
+        float[,] noiseMap = noise.generateNoiseMap(mapChunkSize, mapChunkSize, seed, noiseScale, octaves, persistance, lacunarity, offset); //pass variables into noise generation function
+
+        Color[] colourMap = new Color[mapChunkSize * mapChunkSize];
+        for (int y = 0; y < mapChunkSize; y++)
+            for (int x = 0; x < mapChunkSize; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+                for (int i = 0; i < regions.Length; i++)
+                {
+                    if (currentHeight <= regions[i].height)
+                    {
+                        colourMap[y * mapChunkSize + x] = regions[i].colour; //sets colour of position to the colour of the region based off height
+                        break;
+                    }
+                }
+            }
+
 
         mapDisplay display = FindObjectOfType<mapDisplay>(); //sets display to object with mapDisplay script
-        display.drawNoiseMap(noiseMap); //draws the noise map on the display object
+
+        if (drawMode == DrawMode.NoiseMap)
+            display.drawTexture(textureGenerator.TextureFromHeightMap(noiseMap));
+        else if (drawMode == DrawMode.ColourMap)
+            display.drawTexture(textureGenerator.TextureFromColourMap(colourMap, mapChunkSize, mapChunkSize));
+        else if (drawMode == DrawMode.Mesh)
+            display.DrawMesh(meshGenerator.generateTerrainMesh(noiseMap, meshHeightMultiplier, meshHeightCurve, levelOfDetail), textureGenerator.TextureFromColourMap(colourMap, mapChunkSize, mapChunkSize));
+
     }
+
+    private void OnValidate() //called automatically when a variable is changed in inspector
+    {
+        //clamp variables to avoid errors
+        if (lacunarity < 1)
+            lacunarity = 1;
+        if (octaves < 0)
+            octaves = 0;
+    }
+}
+[System.Serializable]
+public struct TerrainType
+{
+    public string name;
+    public float height;
+    public Color colour;
 }
--- necosystem/Assets/scripts/noise.cs
+++ necosystem/Assets/scripts/noise.cs
@@ -4,24 +4,67 @@ using UnityEngine;
 
 public static class noise
 {
-    public static float[,] generateNoiseMap(int mapWidth, int mapHeight, float scale)
+    public static float[,] generateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset)
     {
         float[,] noiseMap = new float[mapWidth, mapHeight]; //parameters required for noisemap generation
+
+        System.Random prng = new System.Random(seed); //pseudo-random number generator based off an input seed
+        Vector2[] octaveOffsets = new Vector2[octaves];
+        for (int i = 0; i < octaves; i++)
+        {
+            float offsetX = prng.Next(-100000, 100000) + offset.x;
+            float offsetY = prng.Next(-100000, 100000) + offset.y;
+            octaveOffsets[i] = new Vector2(offsetX, offsetY);
+        }
 
         if (scale <= 0) //catch any division errors if scale is <= 0
         {
             scale = 0.0001f;
         }
 
+        float maxNoiseHeight = float.MinValue;
+        float minNoiseHeight = float.MaxValue;
+
+        float halfWidth = mapWidth / 2f; //calculate half values to scale into centre
+        float halfHeight = mapHeight / 2f;
+
         for (int y = 0; y < mapHeight; y++)
             for (int x = 0; x < mapWidth; x++) //for each x and y position
             {
-                float sampleX = x / scale; //make the sample values differ from the int x and y values
-                float sampleY = y / scale;
+                float amplitude = 1;
+                float frequency = 1;
+                float noiseHeight = 0;
 
-                float perlinValue = Mathf.PerlinNoise(sampleX, sampleY); //generate perlinnoise from these values
-                noiseMap[x, y] = perlinValue;
+                for (int i = 0; i < octaves; i++)
+                {
+                    float sampleX = (x - halfWidth) / scale * frequency + octaveOffsets[i].x; //make the sample values differ from the int x and y values, with offsets
+                    float sampleY = (y - halfHeight) / scale * frequency + octaveOffsets[i].y;
+
+                    float perlinValue = Mathf.PerlinNoise(sampleX, sampleY) * 2 - 1; //generate perlin noise from these values
+                    noiseHeight += perlinValue * amplitude;
+
+                    amplitude *= persistance;
+                    frequency *= lacunarity;
+                }
+
+                if (noiseHeight > maxNoiseHeight)
+                {
+                    maxNoiseHeight = noiseHeight;
+                }
+                else if (noiseHeight < minNoiseHeight)
+                {
+                    minNoiseHeight = noiseHeight;
+                }
+                noiseMap[x, y] = noiseHeight;
             }
+
+        //normalise noisemap's x and y values between 0 and 1
+        for (int y = 0; y < mapHeight; y++)
+            for (int x = 0; x < mapWidth; x++) //for each x and y position
+            {
+                noiseMap[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x, y]);
+            }
+                
 
         return noiseMap;
     }
